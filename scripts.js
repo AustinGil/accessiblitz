2b2c09f956ffafe57758112572177dc87c28b379
Austin Gil <austin@stegosource.com>
2018-03-18 10:13:57 -0700

    Adding the JS search functionality

--- scripts.js
+++ scripts.js
@@ -5,28 +5,38 @@ const customCheckboxes = Array.from(
   document.querySelectorAll('.customCheckbox')
 )
 const switches = Array.from(document.querySelectorAll('.switch'))
+const jsSearchers = Array.from(document.querySelectorAll('.js-search'))
 
 menu.addEventListener('click', () => {
   nav.classList.toggle('open')
 })
 
 customRadios.forEach(el => {
-  el.addEventListener('click', e => {
+  el.addEventListener('click', event => {
     const previousEl = el.parentElement.querySelector('.active')
     if (previousEl) previousEl.classList.remove('active')
 
-    e.target.classList.add('active')
+    event.target.classList.add('active')
   })
 })
 
 customCheckboxes.forEach(el => {
-  el.addEventListener('click', e => {
-    e.target.classList.toggle('active')
+  el.addEventListener('click', event => {
+    event.target.classList.toggle('active')
   })
 })
 
 switches.forEach(el => {
-  el.addEventListener('click', e => {
-    e.target.classList.toggle('active')
+  el.addEventListener('click', event => {
+    event.target.classList.toggle('active')
+  })
+})
+
+jsSearchers.forEach(el => {
+  el.addEventListener('keydown', event => {
+    if (event.keyCode === 13 && event.target.value) {
+      alert(`You searched for ${event.target.value}`)
+      event.target.value = ''
+    }
   })
 })
