c52aaa30e892c29bdb2849a19ad7da839c67cc86
Austin Gil <austin@stegosource.com>
2018-04-07 11:55:51 -0700

    Preparing to record

--- README.md
+++ README.md
@@ -17,3 +17,29 @@ Let's make the web more accessible together.
 11. Switch to using semantic markup.
 12. Make external links open in same tab.
 13. Things work with or without JavaScript.
+
+### Helpful Tools & Resources
+
+Understanding the laws and guidelines:
+
+* [Section 508 of the Rehabilitation Act](https://section508.gov/)
+* [Title II of The Americans with Disabilities Act (ADA)](https://www.ada.gov/pcatoolkit/chap5toolkit.htm)
+* [The Web Content Accessibility Guidelines (WCAG)](https://www.w3.org/TR/WCAG21/)
+
+Tools for testing/auditing your site:
+
+* [HTML_CodeSniffer](https://squizlabs.github.io/HTML_CodeSniffer/) - Webapage accessibility auditing bookmarklet
+* [aXe Developer Tools](https://addons.mozilla.org/en-US/firefox/addon/axe-devtools/) - Webapage accessibility auditing browser extension
+* [Chrome Lighthouse](https://developers.google.com/web/tools/lighthouse/) - Webapage accessibility auditing browser feature (Chrome)
+* [Tenon](https://tenon.io/) - Webpage accessibility auditing service
+* [WAVE](http://wave.webaim.org/) - Webpage accessibility auditing service
+* [ChromeVox](https://chrome.google.com/webstore/detail/chromevox/kgejglhpjiefppelpmljglcjbhoiplfn) - Screen reader browser extension (Chrome)
+* [NoCoffee](https://addons.mozilla.org/en-US/firefox/addon/nocoffee/) - Visual-impairment simulating browser extension
+* [Colorblind Web Page Filter](https://www.toptal.com/designers/colorfilter) - Colorblind simulating browser extension
+
+Build tools:
+
+* [Access Lint](https://www.accesslint.com/) – Automatically test your pull requests
+* [Colorsafe](http://colorsafe.co/) – Test font and color combinations
+* [Tanaguru contrast finder](http://contrast-finder.tanaguru.com/) – Color contrast tester
+* [RANDOMA11Y](https://www.randoma11y.com/) – Random, high-contrast color combination
--- scripts.js
+++ scripts.js
@@ -1,44 +1,44 @@
-// const nav = document.querySelector('.nav')
-const menu = document.querySelector('.menu')
-// const customRadios = Array.from(document.querySelectorAll('.customRadio'))
-// const customCheckboxes = Array.from(
-//   document.querySelectorAll('.customCheckbox')
-// )
-// const switches = Array.from(document.querySelectorAll('.switch'))
-const jsSearchers = Array.from(document.querySelectorAll('.js-search'))
+const nav = document.querySelector(".nav")
+const menu = document.querySelector(".menu")
+const customRadios = Array.from(document.querySelectorAll(".customRadio"))
+const customCheckboxes = Array.from(
+  document.querySelectorAll(".customCheckbox")
+)
+const switches = Array.from(document.querySelectorAll(".switch"))
+const jsSearchers = Array.from(document.querySelectorAll(".js-search"))
 
-// menu.addEventListener('click', () => {
-//   nav.classList.toggle('open')
-// })
+menu.addEventListener("click", () => {
+  nav.classList.toggle("open")
+})
 
-// customRadios.forEach(el => {
-//   el.addEventListener('click', event => {
-//     const previousEl = el.parentElement.querySelector('.active')
-//     if (previousEl) previousEl.classList.remove('active')
+customRadios.forEach(el => {
+  el.addEventListener("click", event => {
+    const previousEl = el.parentElement.querySelector(".active")
+    if (previousEl) previousEl.classList.remove("active")
 
-//     event.target.classList.add('active')
-//   })
-// })
+    event.target.classList.add("active")
+  })
+})
 
-// customCheckboxes.forEach(el => {
-//   el.addEventListener('click', event => {
-//     event.target.classList.toggle('active')
-//   })
-// })
+customCheckboxes.forEach(el => {
+  el.addEventListener("click", event => {
+    event.target.classList.toggle("active")
+  })
+})
 
-// switches.forEach(el => {
-//   el.addEventListener('click', event => {
-//     event.target.classList.toggle('active')
-//   })
-// })
+switches.forEach(el => {
+  el.addEventListener("click", event => {
+    event.target.classList.toggle("active")
+  })
+})
 
 function jsSearch(event) {
   if (event.keyCode === 13 && event.target.value) {
     alert(`You searched for ${event.target.value}`)
-    event.target.value = ''
+    event.target.value = ""
   }
 }
 
-// jsSearchers.forEach(el => {
-//   el.addEventListener('keydown', jsSearch)
-// })
+jsSearchers.forEach(el => {
+  el.addEventListener("keydown", jsSearch)
+})
